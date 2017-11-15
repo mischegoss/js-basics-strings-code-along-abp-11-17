c90fbab9e2bade707f13c7d76b764d234c36441b
mischegoss <mischegoss@gmail.com>
2017-11-15 01:30:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const currentUser = 'Grace Hopper';
-onst welcomeMessage = 'Welcome to Flatbook, ';// Write your code in this file!
+const welcomeMessage = 'Welcome to Flatbook, ';// Write your code in this file!
